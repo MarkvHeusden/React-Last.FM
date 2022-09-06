8e2566c0cf92a3d597d34ee911b124f79d01b412
MarkvHeusden <mark.van.heusden@labela.nl>
2022-09-06 10:25:29 +0200

    Fixing duplicate folders

--- src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-const Albums = () => {
-    return ( 
-        <header>
-            <h1>Mijn profiel</h1>
-        </header>
-     )
-}
- 
-export default Albums;
--- src/pages/Trending.js
+++
@@ -1,9 +0,0 @@
-const Trending = () => {
-    return ( 
-        <header>
-            <h1>Trending</h1>
-        </header>
-     )
-}
- 
-export default Trending;
